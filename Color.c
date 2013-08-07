9cc08b49c607192c551f5b9e65c0b080b9dc2178
alimarie <alimarie205@gmail.com>
2013-08-06 18:15:55 -0600

    Update Color.c

--- Color.c
+++ Color.c
@@ -3,7 +3,7 @@
  *  Ali Wood
  *  MEEn 495R
  ***********************************************************************/
-#include <p24F04KA201.h>
+#include <p24F16KA301.h>
 #include <stdlib.h>
 
 // set system clock to Fast RC Oscillator
@@ -15,8 +15,8 @@ _FOSCSEL(FNOSC_FRCPLL);
 #define WHITEBALL _RA4
 
 // calculate boundaries
-static const int ADC_MAX = 1023;
- int Vref; //~2000 mV
+static const int ADC_MAX = 4095;
+int Vref; //~2000 mV
 static const int LOWER = 80;    //in mV
 static const int UPPER = 110;   //in mV
 
@@ -47,37 +47,53 @@ int main() {
 initColorDetection(){
     // set debug pins as outputs
     _TRISA2 = 0;       //pin 7 is purple out
-    _TRISA6 = 0;       //pin 8 (14) is pink out
+    _TRISA6 = 0;       //pin 14 is pink out
     _TRISA4 = 0;       //pin 10 is white out
+
 }
 
 init_ADC(){
-    AD1PCFG = 0xFFF7; //0xFFFB; // sample only AN2 voltage
+    AD1PCFG = 0xFFDF; //0xFFFB; // sample only AN4 voltage  // CHANGE****
+
+    ANSBbits.ANSB4 = 0b1;     // pin6 is analog input
 
     //AD1CON1
     _ADON = 0b1;
-    _ADSIDL = 0b0;
-    _FORM = 0b00;  // Data output as integer
-    _SSRC = 0b111; // auto-convert
-    _ASAM = 0b1;   // sample immediately
+    _ADSIDL = 0b0;  // continue operation in idle mode
+    _MODE12 = 0b1;  // 12-bit A/D operation
+    _FORM = 0b00;   // Data output as integer
+    _SSRC = 0b0111; // auto-convert
+    _ASAM = 0b1;    // sample immediately
+                    // _SAMP 0=holding, 1=sampling
+                    // _DONE 1=completed, 0=notCompleted
 
     //AD1CON2
-    _VCFG = 0b001;  // Vr+ (Vref+ = 2V), Vr- AVss
-    _CSCNA = 0b0;   // use CH0SA bits as mux inputs
-    _BUFS = 0b1;    // filling F8-FF, user access F0-F7
-    _SMPI = 0b1111; // interrupt after 16th sample/convert seq
+    _PVCFG = 0b01;  // Vr+ = external Vref+, 2V
+    _NVCFG = 0b0;   // Vr- = Vss
+                    // _OFFCAL
+    _BUFREGEN = 0b0;// treat buffer as FIFO
+    _CSCNA = 0b0;   // dont scan inputs
+                    // _BUFS 
+    _SMPI = 0b01000;// interrupt after 16th sample/convert seq
     _BUFM = 0b0;    // configured as one 16-word buffer
-    _ALTS = 0b0;    // always use MUXA
+    _ALTS = 0b0;    // always use Sample A inputs
 
-    //AD1CON3 = 0x0D09; // AD Clock as derivative of system clock, 13TAD,16 samples are collected in 1mSec,Fcy =4Mhz 1Tcy=0.26uS
+    //AD1CON3
     _ADRC = 0b0;        // derive clock from system
+                        // _EXTSAM
     _SAMC = 0b00111;    // Auto-Sample Time
     _ADCS = 0b00011111; // A/D Conversion Clock Period
 
-    // AD1CHS  = 0x0005; // Positive sample input channel for MUX A to use AN2,Negative input channel for MUX A to use VR-
-    _CH0NA = 0b0;   // neg mexA input is 0
-    _CH0SA = 0b0011;// pos muxA input is AN2
+    //AD1CON5
+    _ASEN = 0b0;    // disable auto-scan
+    _LPEN = 0b0;    // fullpower mode after scan
+    _BGREQ = 0b0;   // band gap not enabled
+    _ASINT = 0b00;  // no interrupt (threshold detection)
+    _WM = 0b00;     // legacy operation
 
+    //AD1CHS
+    _CH0NA = 0b000;   // neg mexA input is 0
+    _CH0SA = 0b00100; // pos muxA input is AN4        /// CHANGE*****
 
     AD1CSSL = 0x0000; // channel scanning disabled
 }
@@ -98,19 +114,21 @@ ADC_Avg(){
               ADC1BUF7 +
               ADC1BUF8 +
               ADC1BUF9 +
-              ADC1BUFA +
-              ADC1BUFB +
-              ADC1BUFC +
-              ADC1BUFD +
-              ADC1BUFE +
-              ADC1BUFF;
+              ADC1BUF10 +
+              ADC1BUF11 +
+              ADC1BUF12 +
+              ADC1BUF13 +
+              ADC1BUF14 +
+              ADC1BUF15 +
+              ADC1BUF16 +
+              ADC1BUF17;
 
-    int avg = sum/16;
+    int avg = sum/18;
     return avg;
 }
 
 DetectColor(){
-    Vref = 2000;//_LATA0;
+    Vref = 2000;
 
     int ADC_OUT = ADC_Avg();
     int upBound = UPPER*ADC_MAX/Vref;
